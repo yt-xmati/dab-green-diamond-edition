6a28c7a659fa4d3976cc7a8f00eb49b5c15f99de
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-04-24 12:13:10 +0200

    fix!!

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -128,7 +128,7 @@ class MainMenuState extends MusicBeatState
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "D&B Green Diamond Edition v" + shitVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "D&B Green Diamond Edition " + shitVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
